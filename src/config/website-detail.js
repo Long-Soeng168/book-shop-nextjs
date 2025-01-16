f8e3729722220070f44fa91cf9ca88845b8d4b05
Long-Soeng168 <generalpurpose167@gmail.com>
2025-01-16 11:44:11 +0700

    update

---
+++ src/config/website-detail.js
@@ -0,0 +1,5 @@
+const APP_NAME = "Thnal";
+const APP_ADDRESS = "#123, St 456, Sangkat Toul Kork, Khan Toul Kork";
+const APP_EMAIL = "longsoeng@gmail.com";
+const APP_CONTACT = "0987654321";
+export { APP_NAME, APP_ADDRESS, APP_EMAIL, APP_CONTACT };
